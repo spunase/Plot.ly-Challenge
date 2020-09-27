7ce144e11aa9853336e6588d2bb3a8a2a6cea959
CodingGirl <spunase@gmail.com>
2020-09-27 10:39:35 -0500

    comments

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,4 @@
+//Build Metadata
 function buildMetadata(sample) {
   // Using D3 library to read sample.json
   d3.json("samples.json").then((data) => {
@@ -5,7 +6,9 @@ function buildMetadata(sample) {
     var resultsarray = metadata.filter(sampleobject => sampleobject.id == sample);
     var result = resultsarray[0]
     var panel = d3.select("#sample-metadata");
+    // Use `.html("") to Clear any Existing Metadata
     panel.html("");
+     // Use `Object.entries` to Add Each Key & Value Pair to the Panel
     Object.entries(result).forEach(([key, value]) => {
       var row = panel.append("h6");
       row.text(`${key}: ${value} \n`)
@@ -15,6 +18,7 @@ function buildMetadata(sample) {
 
 
 function buildCharts(sample) {
+  // Using D3 library to read sample.json
   d3.json("samples.json").then(function (data) {
 
     for (var i = 0; i < data.samples.length; i++) {
@@ -37,7 +41,7 @@ function buildCharts(sample) {
       }
     };
 
-
+    // data
     var data = [trace1];
 
 
@@ -45,14 +49,17 @@ function buildCharts(sample) {
       xaxis: { title: "OTU ID" },
       title: "Belly Button Bacteria",
     };
-
+    // Render the plot to the div tag with id "bubble"
     Plotly.newPlot("bubble", data, layout);
+
     // Build a Bar chart using the sample data
     d3.json("samples.json").then(function (data) {
+      // Use slice() to Grab the Top 10 sample_values
+      // use reverse() to arrange data
       var values = sample_values.slice(0, 10).reverse();
       var labels = otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse();
       var display = otu_labels.slice(0, 10).reverse();
-
+      // barchart for the sample Data
       var barChart = {
         x: values,
         y: labels,
@@ -60,9 +67,10 @@ function buildCharts(sample) {
         type: "bar",
         orientation: "h"
       };
+      // data
       var data = [barChart];
+      // Apply the group bar mode to the layout
       var layout = {
-        title: "bar",
         margin: {
           l: 100,
           r: 100,
@@ -70,6 +78,7 @@ function buildCharts(sample) {
           b: 100
         }
       };
+      // Render the plot to the div tag with id "bar"
       Plotly.newPlot("bar", data, layout);
     });
   });
