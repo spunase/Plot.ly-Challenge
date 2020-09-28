59b412e7632e20babacd55f8545eb91c5e9adbc4
CodingGirl <spunase@gmail.com>
2020-09-28 18:04:50 -0500

    working Gauge Chart

--- StarterCode/static/js/Bonus.js
+++ StarterCode/static/js/Bonus.js
@@ -2,129 +2,93 @@ function buildGauge(sample) {
     d3.json("samples.json").then((data) => {
         for (var i = 0; i < data.metadata.length; i++) {
             if (data.metadata[i].id == sample) {
-              
-              var wfreq = data.metadata[i].wfreq;
-              
+
+                var wfreq = data.metadata[i].wfreq;
+                var level = wfreq * 20;
+                // Trigonometry to Calculate Meter Point
+                var degrees = 180 - level;
+                var radius = 0.5;
+                var radians = (degrees * Math.PI) / 180;
+                var x = radius * Math.cos(radians);
+                var y = radius * Math.sin(radians);
+
+                // Path May Have to Change to Create a Better Triangle
+                var mainPath = "M-.0 -0.05 L  .0 0.05 L";
+                var pathX = String(x);
+                var space = " ";
+                var pathY = String(y);
+                var pathEnd = " Z";
+                var path = mainPath.concat(pathX, space, pathY, pathEnd);
+                console.log(path);
+                var data = [
+                    {
+                        type: "scatter",
+                        x: [0],
+                        y: [0],
+                        marker: { size: 12, color: "850000" },
+                        showlegend: false,
+                        text: level,
+                        hoverinfo: "text+name"
+                    },
+                    {
+                        values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+                        rotation: 90,
+                        text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+                        textinfo: "text",
+                        textposition: "inside",
+                        marker: {
+                            colors: [
+                                "rgba(0,105,11,.5)",
+                                "rgba(10,120,22,.5)",
+                                "rgba(14,127,0,.5)",
+                                "rgba(110,154,22,.5)",
+                                "rgba(170,202,42,.5)",
+                                "rgba(202,209,95,.5)",
+                                "rgba(210,206,145,.5)",
+                                "rgba(232,226,202,.5)",
+                                "rgba(240, 230,215,.5)",
+                                "rgba(255,255,255,0)"
+                            ]
+                        },
+                        labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+                        hoverinfo: "label",
+                        hole: 0.5,
+                        type: "pie",
+                        showlegend: false
+                    }
+                ]
+
+                var layout = {
+                    shapes: [
+                        {
+                            type: "path",
+                            path: path,
+                            fillcolor: "850000",
+                            line: {
+                                color: "850000"
+                            }
+                        }
+                    ],
+                    title: "Belly Button Washing Frequency <br> Scrubs per Week",
+                    height: 500,
+                    width: 500,
+                    xaxis: {
+                        zeroline: false,
+                        showticklabels: false,
+                        showgrid: false,
+                        range: [-1, 1]
+                    },
+                    yaxis: {
+                        zeroline: false,
+                        showticklabels: false,
+                        showgrid: false,
+                        range: [-1, 1]
+                    }
+                }
+                var GAUGE = document.getElementById("gauge");
+                Plotly.newPlot(GAUGE, data, layout
+                )
             }
         }
-    
-    // var traceA =  
-    //     {
-    //     type: "pie",
-    //     showlegend: false,
-    //     hole: 0.4,
-    //     rotation: 90,
-    //     values: [100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100],
-    //     text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-    //     direction: "clockwise",
-    //     textinfo: "text",
-    //     textposition: "inside",
-    //     marker: {
-    //         colors: [
-    //             "rgba(240, 230,215,.5)",
-    //             "rgba(232,226,202,.5)",
-    //             "rgba(210,206,145,.5)",
-    //             "rgba(202,209,95,.5)",
-    //             "rgba(170,202,42,.5)",
-    //             "rgba(110,154,22,.5)",
-    //             "rgba(14,127,0,.5)",
-    //             "rgba(10,120,22,.5)",
-    //             "rgba(0,105,11,.5)",
-    //             "rgba(255,255,255,0)"
-    //         ]
-    //     },
-    //     labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-    //     hoverinfo: "label"
-    //     };
-    
-    // // Needle
-    // var degrees = 180 - level, radius = .9;
-    // var radians = degrees * Math.PI / 180;
-    // var x = radius * Math.cos(radians);
-    // var y = radius * Math.sin(radians);
-    // // Path: may have to change to create a better triangle
-    // var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
-    //     pathX = String(x),
-    //     space = ' ',
-    //     pathY = String(y),
-    //     pathEnd = ' Z';
-    // var path = mainPath.concat(pathX,space,pathY,pathEnd);
-    // x1Pos = ["0.1","0.2", "0.3", "0.4", "0.5","0.6", "0.7", "0.8", "0.9"];
-    // y1Pos = ["0.55","0.65", "0.7", "0.75", "0.7","0.65", "0.6", "0.55", "0.52"];
-    
-    // x1val = x1Pos[wfreq];
-    // y1val = y1Pos[wfreq];
-    // console.log(x1val, y1val)
-    // var layout = {
-    // shapes:[{
-    //         // type: 'path',
-    //         // path: path,
-    //         // fillcolor: "850000",
-    //         // line: {
-    //         //     color: "850000"
-    //         type: 'line',
-    //         x0: 0.5,
-    //         y0: 0.5,
-    //         x1: x1val,
-    //         y1: y1val,
-    //         line: {
-    //         color: 'black',
-    //         width: 3
-    //         }
-    //   }],
-            
-    // title: "Belly Button Washing Frequency <br> Scrubs per Week",
-    // xaxis: {
-    //     zeroline:false,
-    //     showticklabels: false,
-    //     showgrid: false,
-    //     range: [-1, 1]
-    // },
-    // yaxis: {
-    //     zeroline: false,
-    //     showticklabels: false,
-    //     showgrid: false,
-    //     range: [-1, 1]
-    // }
-
-    // };
-
-    // var data = [traceA];
-
-    var data = [
-        {
-          type: "indicator",
-          mode: "gauge",
-          title: { text: "Belly Button Washing Frequency <br> Scrubs per Week", font: { size: 24 } },
-          gauge: {
-            axis: { range: [null, 9], tickwidth: 5, ticks: "inside", tickvals: [wfreq], ticklen: 225, tickcolor: "black" },
-            bgcolor: "white",
-            borderwidth: 2,
-            bordercolor: "gray",
-            steps: [
-              { range: [0, 1], color: "rgba(240, 230,215,.5)" },
-              { range: [1, 2], color: "rgba(232,226,202,.5)" },
-              { range: [2, 3], color: "rgba(210,206,145,.5)" },
-              { range: [3, 4], color: "rgba(202,209,95,.5)" },
-              { range: [4, 5], color: "rgba(170,202,42,.5" },
-              { range: [5, 6], color: "rgba(110,154,22,.5)" },
-              { range: [6, 7], color: "rgba(14,127,0,.5)" },
-              { range: [7, 8], color: "rgba(10,120,22,.5)" },
-              { range: [8, 9], color: "rgba(0,105,11,.5)" }
-            ],
-          }
-        }
-      ];
-      
-      var layout = {
-        width: 500,
-        height: 400,
-        margin: { t: 25, r: 25, l: 25, b: 25 },
-        font: { color: "black", family: "Arial" }
-      };
-      
-    // var gaugeDiv = document.getElementById("gauge");
-
-    Plotly.plot(gauge, data, layout);
-        });
-}
+    }
+)}
