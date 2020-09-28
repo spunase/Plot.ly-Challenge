8d56716c1195ca36ae8c9956308e86627807f847
CodingGirl <spunase@gmail.com>
2020-09-28 15:33:28 -0500

    needle

--- StarterCode/static/js/Bonus.js
+++ StarterCode/static/js/Bonus.js
@@ -9,33 +9,35 @@ function buildGauge(sample) {
         }
     
     level = wfreq * 20
-    var traceA = {
-    type: "pie",
-    showlegend: false,
-    hole: 0.4,
-    rotation: 90,
-    values: [100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100],
-    text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-    direction: "clockwise",
-    textinfo: "text",
-    textposition: "inside",
-    marker: {
-        colors: [
-            "rgba(240, 230,215,.5)",
-            "rgba(232,226,202,.5)",
-            "rgba(210,206,145,.5)",
-            "rgba(202,209,95,.5)",
-            "rgba(170,202,42,.5)",
-            "rgba(110,154,22,.5)",
-            "rgba(14,127,0,.5)",
-            "rgba(10,120,22,.5)",
-            "rgba(0,105,11,.5)",
-            "rgba(255,255,255,0)"
-        ]
-    },
-    labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-    hoverinfo: "label"
-    };
+    var traceA =  
+        {
+        type: "pie",
+        showlegend: false,
+        hole: 0.4,
+        rotation: 90,
+        values: [100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100],
+        text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+        direction: "clockwise",
+        textinfo: "text",
+        textposition: "inside",
+        marker: {
+            colors: [
+                "rgba(240, 230,215,.5)",
+                "rgba(232,226,202,.5)",
+                "rgba(210,206,145,.5)",
+                "rgba(202,209,95,.5)",
+                "rgba(170,202,42,.5)",
+                "rgba(110,154,22,.5)",
+                "rgba(14,127,0,.5)",
+                "rgba(10,120,22,.5)",
+                "rgba(0,105,11,.5)",
+                "rgba(255,255,255,0)"
+            ]
+        },
+        labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+        hoverinfo: "label"
+        };
+    
     // Needle
     var degrees = 180 - level, radius = .9;
     var radians = degrees * Math.PI / 180;
@@ -48,7 +50,12 @@ function buildGauge(sample) {
         pathY = String(y),
         pathEnd = ' Z';
     var path = mainPath.concat(pathX,space,pathY,pathEnd);
-
+    x1Pos = ["0.1","0.2", "0.3", "0.4", "0.5","0.6", "0.7", "0.8", "0.9"];
+    y1Pos = ["0.55","0.65", "0.7", "0.75", "0.7","0.65", "0.6", "0.55", "0.52"];
+    
+    x1val = x1Pos[wfreq];
+    y1val = y1Pos[wfreq];
+    console.log(x1val, y1val)
     var layout = {
     shapes:[{
             // type: 'path',
@@ -59,8 +66,8 @@ function buildGauge(sample) {
             type: 'line',
             x0: 0.5,
             y0: 0.5,
-            x1: 0.7,
-            y1: 0.7,
+            x1: x1val,
+            y1: y1val,
             line: {
             color: 'black',
             width: 3
@@ -84,8 +91,8 @@ function buildGauge(sample) {
     };
 
     var data = [traceA];
-    var gaugeDiv = document.getElementById("gauge");
+    // var gaugeDiv = document.getElementById("gauge");
 
-    Plotly.plot(gaugeDiv, data, layout);
+    Plotly.plot(gauge, data, layout);
         });
 }
