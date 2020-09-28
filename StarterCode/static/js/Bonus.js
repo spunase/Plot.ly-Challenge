a1b28060bf8f5d10ac15ed8807636fda3465bcb9
CodingGirl <spunase@gmail.com>
2020-09-28 14:04:48 -0500

    gauge chart

--- StarterCode/static/js/Bonus.js
+++ StarterCode/static/js/Bonus.js
@@ -4,82 +4,81 @@ function buildGauge(sample) {
             if (data.metadata[i].id == sample) {
               
               var wfreq = data.metadata[i].wfreq;
-
+              console.log(wfreq)
             }
         }
     
     level = wfreq * 20
-var traceA = {
-  type: "pie",
-  showlegend: false,
-  hole: 0.4,
-  rotation: 90,
-  values: [100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100],
-  text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-  direction: "clockwise",
-  textinfo: "text",
-  textposition: "inside",
-  marker: {
-    colors: [
-        "rgba(240, 230,215,.5)",
-        "rgba(232,226,202,.5)",
-        "rgba(210,206,145,.5)",
-        "rgba(202,209,95,.5)",
-        "rgba(170,202,42,.5)",
-        "rgba(110,154,22,.5)",
-        "rgba(14,127,0,.5)",
-        "rgba(10,120,22,.5)",
-        "rgba(0,105,11,.5)",
-        "rgba(255,255,255,0)"
+    var traceA = {
+    type: "pie",
+    showlegend: false,
+    hole: 0.4,
+    rotation: 90,
+    values: [100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100/9, 100],
+    text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+    direction: "clockwise",
+    textinfo: "text",
+    textposition: "inside",
+    marker: {
+        colors: [
+            "rgba(240, 230,215,.5)",
+            "rgba(232,226,202,.5)",
+            "rgba(210,206,145,.5)",
+            "rgba(202,209,95,.5)",
+            "rgba(170,202,42,.5)",
+            "rgba(110,154,22,.5)",
+            "rgba(14,127,0,.5)",
+            "rgba(10,120,22,.5)",
+            "rgba(0,105,11,.5)",
+            "rgba(255,255,255,0)"
+        ]
+    },
+    labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+    hoverinfo: "label"
+    };
+    // Needle
+    var degrees = 50, radius = .9;
+    var radians = degrees * Math.PI / 180;
+    var x = -1 * radius * Math.cos(radians)* wfreq;
+    var y = radius * Math.sin(radians);
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
 
-    ]
-  },
-  labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-  hoverinfo: "label"
-};
+    var layout = {
+    shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: "850000",
+            line: {
+                color: "850000"
+            }
+        } 
+    ],      
+            
+    title: "Belly Button Washing Frequency <br> Scrubs per Week",
+    xaxis: {
+        zeroline:false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1, 1]
+    },
+    yaxis: {
+        zeroline: false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1, 1]
+    }
 
-var degrees = 180 - level, radius = .6;
-var radians = degrees * Math.PI / 180;
-var x = radius * Math.cos(radians);
-var y = radius * Math.sin(radians);
-// Path: may have to change to create a better triangle
-var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
-     pathX = String(x),
-     space = ' ',
-     pathY = String(y),
-     pathEnd = ' Z';
-var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    };
 
-var layout = {
-  shapes:[{
-        type: 'path',
-        path: path,
-        fillcolor: "850000",
-        line: {
-            color: "850000"
-        }
-    } 
-],      
-        
-  title: "Belly Button Washing Frequency <br> Scrubs per Week",
-  xaxis: {
-    zeroline:false,
-    showticklabels: false,
-    showgrid: false,
-    range: [-1, 1]
-},
-yaxis: {
-    zeroline: false,
-    showticklabels: false,
-    showgrid: false,
-    range: [-1, 1]
-}
+    var data = [traceA];
+    var gaugeDiv = document.getElementById("gauge");
 
-};
-
-var data = [traceA];
-var gaugeDiv = document.getElementById("gauge");
-
-Plotly.plot(gaugeDiv, data, layout);
-    });
+    Plotly.plot(gaugeDiv, data, layout);
+        });
 }
